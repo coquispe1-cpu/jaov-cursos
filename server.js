9187c4c132cab584339e1bf8e285a60556b280d5
coquispe1-cpu <coquispe1@gmail.com>
2025-08-22 22:39:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-// Ruta principal
 app.get('/', (req, res) => {
   res.send('Servidor funcionando en Render con CommonJS');
 });
 
-// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
